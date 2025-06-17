02846a0d45e0485ac67344523e5787da6f6bef8e
Susu-spec <suleimansuwaibat@gmail.com>
2025-06-17 22:20:27 +0100

    feat(app): adds header and main to main App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-function App() {
+import RiseLogo from "./assets/rise-logo.png"
+import Dashboard from "./dashboard-page"
 
+function App() {
   return (
     <div className="min-h-screen w-full">
-
+      <header className="w-full px-10 py-6">
+        <img src={RiseLogo} alt="Risevest Technologies logo" />
+      </header>
+      <main className="w-full max-w-[90%] py-10">
+        <Dashboard />
+      </main>
     </div>
   )
 }
