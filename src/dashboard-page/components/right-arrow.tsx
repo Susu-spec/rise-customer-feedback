a106d62e2929060d094f1e4286bc2987b7350920
Susu-spec <suleimansuwaibat@gmail.com>
2025-06-18 21:07:59 +0100

    feat(icons): adds arrow icon components

---
+++ src/dashboard-page/components/right-arrow.tsx
@@ -0,0 +1,14 @@
+export default function RightArrow() {
+    return (
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_19_1932)">
+            <path d="M10.9764 9.99999L6.85144 5.87499L8.02977 4.69666L13.3331 9.99999L8.02977 15.3033L6.85144 14.125L10.9764 9.99999Z" fill="currentColor"/>
+            </g>
+            <defs>
+            <clipPath id="clip0_19_1932">
+            <rect width="20" height="20" fill="white"/>
+            </clipPath>
+            </defs>
+        </svg>
+    )
+}
