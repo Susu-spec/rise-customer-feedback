77aa227b7b8d508d438787131154711c2d970348
Susu-spec <suleimansuwaibat@gmail.com>
2025-06-18 22:42:15 +0100

    fix(feedback): adds check for number data and fixes avatar initials handling

--- src/dashboard-page/components/feedback-card.tsx
+++ src/dashboard-page/components/feedback-card.tsx
@@ -1,46 +1,53 @@
 import emailIcon from "@/assets/email-icon.svg";
-import phoneIcon from "@/assets/phone-icon.svg";
-import reportIcon from "@/assets/report-icon.svg";
+import reportIcon from "@/assets/phone-icon.svg";
+import phoneIcon from "@/assets/report-icon.svg";
 
-type FeedbackValueProp = {
+export type FeedbackValueProp = {
     name: string,
     email: string,
     number: string,
-    feedbackType: string,
+    type: "bug" | "feature" | "other",
     message: string
 }
 
 export default function FeedbackCard({
-    name, email, number, feedbackType, message
+    name, email, number, type, message
 }: FeedbackValueProp) {
     
     const pair = colors[getRandomInt(0, colors.length - 1)];
 
     return (
-        <div className="rounded-2xl bg-white p-3 flex flex-col items-center justify-center gap-2">
+        <div className="rounded-2xl bg-white border border-[#EAECF0] p-3 flex flex-col items-center justify-center gap-2">
             <div className="w-full flex items-center gap-2">
-                <div className={`h-8 w-8 text-center bg-[${pair.background}] text-[${pair.text}]`}>
+                <div className={`h-8 w-8 text-center rounded-[50%] font-medium flex items-center justify-center`}
+                    style={{
+                        backgroundColor: pair.background,
+                        color: pair.text,
+                    }}
+                >
                     {avatarInitials(name)}
                 </div>
-                <p className="font-medium text-black">{name}</p>
+                <p className="font-medium text-black truncate max-w-[20rem] capitalize">{name}</p>
             </div>
-            <div className="flex items-center gap-2">
+            <div className="w-full flex items-center gap-2">
                 <img src={emailIcon} alt="Email logo" />
-                <p className="text-[#555B64] text-sm">
+                <p className="text-[#555B64] text-sm truncate max-w-[20rem]">
                     {email}
                 </p>
             </div>
-            <div className="flex items-center gap-2">
-                <img src={phoneIcon} alt="Phone logo" />
-                <p className="text-[#555B64] text-sm">
-                    {number}
-                </p>
-            </div>
-            <div className="flex items-center gap-2">
+            {number && 
+                <div className="w-full flex items-center gap-2">
+                    <img src={phoneIcon} alt="Phone logo" />
+                    <p className="text-[#555B64] text-sm">
+                        {number}
+                    </p>
+                </div>
+            }
+            <div className="w-full flex items-center gap-2">
                 <img src={reportIcon} alt="Phone logo" />
                 <div className="flex flex-col gap-1">
                     <p className="text-black font-medium text-sm">
-                        {feedbackType}
+                        {type}
                     </p>
                     <p className="text-[#555B64] text-sm">
                         {message}
@@ -52,14 +59,18 @@ export default function FeedbackCard({
 }
 
 function avatarInitials(name: string): string {
-  const words = name.trim().split(/\s+/);
-  if (words.length === 0) return '';
-
-  const first = words[0].charAt(0);
-  const last = words[words.length - 1].charAt(0);
-
-  return (first + last).toUpperCase();
+    const words = name.trim().split(/\s+/);
+    const validWords = words.filter(word => /^[A-Za-z]/.test(word));
+    
+    if (validWords.length === 0) return '';
+    if (validWords.length === 1) return validWords[0].charAt(0).toUpperCase();
+    
+    const first = validWords[0].charAt(0).toUpperCase();
+    const last = validWords[validWords.length - 1].charAt(0).toUpperCase();
+    
+    return first + last;
 }
+
 
 const colors = [
     { background: '#F5F1FE', text: '#A479FF'},
