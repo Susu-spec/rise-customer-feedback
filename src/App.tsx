910c5998d7ed254b993883b82e09e2ddfe6e1270
Susu-spec <suleimansuwaibat@gmail.com>
2025-06-18 21:03:10 +0100

    fix(layout): fixes main layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import Dashboard from "./dashboard-page"
 function App() {
   return (
     <div className="min-h-screen w-full">
-      <header className="w-full px-10 py-6">
+      <header className="w-full px-10 py-6 bg-white border-b border-[#EAECF0]">
         <img src={RiseLogo} alt="Risevest Technologies logo" />
       </header>
-      <main className="w-full max-w-[90%] py-10">
+      <main className="w-full px-10 py-10 !mx-auto">
         <Dashboard />
       </main>
     </div>
