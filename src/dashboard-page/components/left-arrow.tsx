a8a48fe34010e99ab8d0969dc067b06adc180f7a
Susu-spec <suleimansuwaibat@gmail.com>
2025-06-18 22:45:29 +0100

    feat(icons): adds pagination arrow icons

--- src/dashboard-page/components/left-arrow.tsx
+++ src/dashboard-page/components/left-arrow.tsx
@@ -1,7 +1,7 @@
 export default function LeftArrow() {
     return (
         <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clip-path="url(#clip0_19_1930)">
+            <g clipPath="url(#clip0_19_1930)">
             <path d="M9.02329 9.99999L13.1483 14.125L11.97 15.3033L6.66663 9.99999L11.97 4.69666L13.1483 5.87499L9.02329 9.99999Z" fill="currentColor"/>
             </g>
             <defs>
--- src/dashboard-page/components/right-arrow.tsx
+++ src/dashboard-page/components/right-arrow.tsx
@@ -1,7 +1,7 @@
 export default function RightArrow() {
     return (
         <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clip-path="url(#clip0_19_1932)">
+            <g clipPath="url(#clip0_19_1932)">
             <path d="M10.9764 9.99999L6.85144 5.87499L8.02977 4.69666L13.3331 9.99999L8.02977 15.3033L6.85144 14.125L10.9764 9.99999Z" fill="currentColor"/>
             </g>
             <defs>
